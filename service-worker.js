c59dc257a559e87fd5d46c35ac475e906a28d693
kalojandg <kalojandg@users.noreply.github.com>
2025-08-12 15:03:15 +0300

    cache name

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "monk-lite-v2.1";
+const CACHE = "monk-v2.1";
 const ASSETS = ["./","./index.html","./app.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install", e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", e=>{ e.waitUntil(self.clients.claim()); });
